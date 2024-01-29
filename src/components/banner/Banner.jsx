c4e1d9f960eccd2e550a46a290f88e3806112b23
andersonsouzacardoso <andersonfdl@gmail.com>
2024-01-29 18:56:59 -0300

    mandando o segundo projeto para o portifólio

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -7,7 +7,7 @@ export default function Banner(){
   <img src="https://media1.giphy.com/media/qgQUggAC3Pfv687qPC/giphy.gif" alt="" />
   <div class="card__content">
     <p class="card__title">ANDERSON SOUZA</p>
-    <p class="card__description">Sou um desenvolvedor front-end trabalho com Javascript e React.js.</p>
+    <p class="card__description">Desenvolvedor front-end trabalho com Javascript e React.js.</p>
   </div>
 </div>
 
--- src/components/paginas/projetos/Projetos.jsx
+++ src/components/paginas/projetos/Projetos.jsx
@@ -12,11 +12,26 @@ export default function Projetos(){
             <div className="projetos">
                 <div className="projeto1">
                 <img className="galaxy" src={projeto1} alt="imagem do projeto app" />
-                <a className="link" href="https://andersonsouzacardoso.github.io/App-producao/?#" target="_blank">Click Aqui</a>
+                <hr width ="299px"/>
+                <p className="description">Um aplicativo que tem como função auxiliar no trabalho de produção do profissional da área de pizzaria, Masseiro/Pizzaiolo.
+                 Essa ferramenta faz o planejamento da produção do dia, indica quais produtos serão confeccionados e os números de espaços ou carrinhos de massa serão ultilizados.</p>
+                <a className="link" href="https://andersonsouzacardoso.github.io/App-producao/?#" target="_blank">Clique Aqui</a>
                 
                 </div>
                 <div className="projeto2">
                     <img className="Tv" src={projeto2} alt="imagem da tv retro" />
+                    <hr width ="299px"/>
+                    <p className="description">Este foi um projeto criado para praticar meus estudos em CSS e Javascript.
+                    Desafios: Usar o flebox para alinhar e organizar telas e botões com CSS e usar Funções e eventos de clique,
+                    para que o Javascript possa dar vida ao projeto.</p>
+                    <h5>MANUAL DA TV</h5>
+                    <ol>
+                    <li className="manual">Aperte o botão ON</li>
+                    <li className="manual">Passe os canais apertando os botões 12, 10, 07, 05, 04</li>
+                    <li className="manual">Aumente e diminua o volume da TV</li>
+                    <li className="manual">desligue a TV com o botão OFF</li>
+                    </ol>
+                    
                     <a  className="link"href="https://andersonsouzacardoso.github.io/TV-Retro-1984/tv1984.html" target="_blank">Clique Aqui</a>
                     
                 </div>
